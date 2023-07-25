778f698edb38e27129ba167c2e572c93c69dd528
Amirhossein <Amirmemool12@gmail.com>
2023-07-25 17:51:30 +0330

    :hammer: chore: add return type for the method of Request class

--- core/Request.php
+++ core/Request.php
@@ -4,16 +4,14 @@ namespace App\core;
 
 class Request
 {
-    public function getPath()
+    public function getPath(): string
     {
         $path = $_SERVER["REQUEST_URI"];
         return substr($path, 0, strpos($path, "?") ?: strlen($path));
     }
 
-    public function getMethod()
+    public function getMethod(): string
     {
         return $_SERVER["REQUEST_METHOD"];
     }
-
-
 }
