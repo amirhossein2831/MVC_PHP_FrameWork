e380257a696f1104f1837185c735e85f30f7be54
Amirhossein <Amirmemool12@gmail.com>
2023-07-25 22:52:09 +0330

    :truck: rename: rename the Controller

--- Controller/SiteController.php
+++ Controller/SiteController.php
@@ -1,18 +1,24 @@
 <?php
 namespace App\Controller;
 use App\core\Application;
+use App\core\Request;
 
 class Controller
 {
 
     public function home()
     {
-        $this->renderView( 'home',  'main');
+        $this->renderView( 'home',  'main');            //if you want to pass value to view pass it to render view then pass it to contentofView
     }
 
     public function contact()
     {
         $this->renderView('contact','main');
+    }
+
+    public function handleContact(Request $request)
+    {
+        var_dump($request->getBody());
     }
 
     public static function notFount()
