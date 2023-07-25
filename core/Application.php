58c8f80c1e37c3f0803e28508f3cf0e1b4693487
Amirhossein <Amirmemool12@gmail.com>
2023-07-25 20:11:22 +0330

    :hammer: chore:create instance of the response in Application and pass them to Router and use them

--- core/Application.php
+++ core/Application.php
@@ -8,11 +8,13 @@ class Application
 {
     public const ROOT = '/opt/lampp/htdocs/project/PHPFrameWork';
     private Request $request;
+    private Response $response;
     private Router $router;
 
     public function __construct(){
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response = new Response();
+        $this->router = new Router($this->request,$this->response);
         $this->initialRouter();
     }
 
@@ -36,6 +38,10 @@ class Application
         $controller = new Controller();
         $this->getRouter()->get('/',[$controller,'home']);
         $this->getRouter()->get('/contact',[$controller,'contact']);
+    }
+
+    public final function getResponse(){
+        return $this->response;
     }
 
 }
