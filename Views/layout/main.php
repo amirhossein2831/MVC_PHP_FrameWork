0b117fe86559d4b7fa032de8efa3ff04df1cf503
Amirhossein <Amirmemool12@gmail.com>
2023-07-25 19:54:36 +0330

    :hammer: chore: change the value with strign

--- Views/layout/main.php
+++ Views/layout/main.php
@@ -28,9 +28,7 @@
         </div>
     </nav>
 
-    <?php
-    echo $content;
-    ?>
+    {{content}}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
