9536a255c92f8143ae800a667256258008cd7f1f
Amirhossein <Amirmemool12@gmail.com>
2023-07-27 00:50:42 +0330

    :heavy_plus_sign: feat: add migration.php file to run migrations

---
+++ migrations.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use App\core\Application;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dns'=>$_ENV['DB_DNS'],
+        'user'=>$_ENV['DB_USER'],
+        'password'=>$_ENV['DB_PASSWORD']
+    ]
+];
+$app = new Application($config);
+
+$app->getDataBase()->applyMigration();
+
