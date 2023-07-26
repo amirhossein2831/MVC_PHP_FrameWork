c263f0d12391fb128903f032566274df923a11d8
Amirhossein <Amirmemool12@gmail.com>
2023-07-26 14:34:26 +0330

    :hammer: chore:add field and getter of the RegisterModels

--- Models/RegisterModel.php
+++ Models/RegisterModel.php
@@ -1,6 +1,43 @@
 <?php
+
 namespace App\Models;
 class RegisterModel
 {
+    private string $firstName;
+    private string $lastName;
+    private string $password;
+    private string $confirmPassword;
+    private string $email;
+
+
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+
+    public function getConfirmPassword(): string
+    {
+        return $this->confirmPassword;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
 
 }
