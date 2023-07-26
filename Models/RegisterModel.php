1d3c8548b569f5edc854bc1b35fbebf756e78ec8
Amirhossein <Amirmemool12@gmail.com>
2023-07-26 21:01:25 +0330

    :recycle: chore: change access modifier of field and create constructor

--- Models/RegisterModel.php
+++ Models/RegisterModel.php
@@ -1,16 +1,26 @@
 <?php
 
 namespace App\Models;
+
 use App\core\BaseModel;
 use App\Rule\RegisterRule;
 
 class RegisterModel extends BaseModel
 {
-    protected string $firstName;
-    protected string $lastName;
-    protected string $password;
-    protected string $confirmPassword;
-    protected string $email;
+    public string $firstName;
+    public string $lastName;
+    public string $password;
+    public string $confirmPassword;
+    public string $email;
+
+    public function __construct()
+    {
+        $this->firstName = '';
+        $this->lastName = '';
+        $this->password = '';
+        $this->confirmPassword = '';
+        $this->email = '';
+    }
 
     public function validate(): bool
     {
@@ -20,20 +30,20 @@ class RegisterModel extends BaseModel
             foreach ($rules as $rule) {
                 $ruleName = is_string($rule) ? $rule : $rule[0];
 
-                if($ruleName === RegisterRule::REQUIRED_FIELD && !$value){
+                if ($ruleName === RegisterRule::REQUIRED_FIELD && !$value) {
                     $this->addError($attribute, RegisterRule::REQUIRED_FIELD);
                 }
                 if ($ruleName === RegisterRule::EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                    $this->addError($attribute,RegisterRule::EMAIL);
+                    $this->addError($attribute, RegisterRule::EMAIL);
                 }
                 if ($ruleName === RegisterRule::MIN_LENGTH && strlen($value) < $rule['min']) {
-                    $this->addError($attribute,RegisterRule::MIN_LENGTH,$rule);
+                    $this->addError($attribute, RegisterRule::MIN_LENGTH, $rule);
                 }
                 if ($ruleName === RegisterRule::MAX_LENGTH && strlen($value) > $rule['max']) {
-                    $this->addError($attribute,RegisterRule::MAX_LENGTH,$rule);
+                    $this->addError($attribute, RegisterRule::MAX_LENGTH, $rule);
                 }
-                if ($ruleName === RegisterRule::MATH_FIELD && $value !== $this->{$rules['match']}) {
-                    $this->addError($attribute,RegisterRule::MATH_FIELD,$rule);
+                if ($ruleName === RegisterRule::MATCH_FIELD && $value != $this->{$rule['match']}) {
+                    $this->addError($attribute, RegisterRule::MATCH_FIELD, $rule);
                 }
             }
         }
@@ -44,37 +54,9 @@ class RegisterModel extends BaseModel
     {
         //TODO: save the date in DB
     }
+
     public function rules(): array
     {
         return RegisterRule::rules();
     }
-
-    public function getFirstName(): string
-    {
-        return $this->firstName;
-    }
-
-
-    public function getLastName(): string
-    {
-        return $this->lastName;
-    }
-
-
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-
-    public function getConfirmPassword(): string
-    {
-        return $this->confirmPassword;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
 }
