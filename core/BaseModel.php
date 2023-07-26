74079214fcfaf77d3df7658e667f2aa5f306eeb0
Amirhossein <Amirmemool12@gmail.com>
2023-07-26 15:06:22 +0330

    :hammer: chore: implement loadDate method

--- core/BaseModel.php
+++ core/BaseModel.php
@@ -5,11 +5,14 @@ namespace App\core;
 abstract class BaseModel
 {
 
-    public function loadDate()
+    public function loadDate($date)
     {
-
+        foreach ($date as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
     }
-
     public function validate()
     {
 
