9c259b5ea39a8e0d78aedf5525080fb0d53a0d83
Amirhossein <Amirmemool12@gmail.com>
2023-07-26 22:17:51 +0330

    :hammer: chore: declare up and down method

--- Migrations/M001_Initial.php
+++ Migrations/M001_Initial.php
@@ -3,5 +3,13 @@ namespace App\Migrations;
 
 class M001_Initial
 {
-   
+    public function up()
+    {
+        echo "up migration";
+    }
+
+    public function down()
+    {
+        echo "donw migration";
+    }
 }
