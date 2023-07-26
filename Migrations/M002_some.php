3ff3542ab53890c778e34b6b4ab8097f5537a715
Amirhossein <Amirmemool12@gmail.com>
2023-07-27 00:49:59 +0330

    :heavy_plus_sign: chore: add anoter Migration class

--- Migrations/M002_some.php
+++ Migrations/M002_some.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace App\Migrations;
+
+class M002_some
+{
+    public function up()
+    {
+        echo "up migration";
+    }
+
+    public function down()
+    {
+        echo "down migration";
+    }
+}
