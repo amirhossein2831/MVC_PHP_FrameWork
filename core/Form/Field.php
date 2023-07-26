3212750c1c99d26e735467e3400668e16164f435
Amirhossein <Amirmemool12@gmail.com>
2023-07-26 21:03:35 +0330

    :hammer: chore: create Input type of the field as const

--- core/Form/Field.php
+++ core/Form/Field.php
@@ -2,7 +2,25 @@
 
 namespace App\core\Form;
 
+use App\Models\RegisterModel;
+
 class Field
 {
+    public const TEXT_FIELD = 'text';
+    public const PASSWORD_FIELD = 'password';
+    public const CHECKBOX_FIELD = 'checkbox';
+    public const RADIO_BUTTON = 'radio';
+    public const SUBMIT_BUTTON = 'submit';
+    public const FILE_FIELD = 'file';
+    public const HIDDEN_FIELD = 'hidden';
+    public const NUMBER_FIELD = 'number';
+    public const EMAIL_FIELD = 'email';
+    public const DATE_FIELD = 'date';
+    public const RANGE_FIELD = 'range';
+    public const COLOR_FIELD = 'color';
 
+    public RegisterModel $model;
+    public string $attribute;
+
+ 
 }
