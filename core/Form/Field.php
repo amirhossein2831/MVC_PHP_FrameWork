11802845a03fb6f2b9412c6ea32365570499fb5f
Amirhossein <Amirmemool12@gmail.com>
2023-07-26 21:04:03 +0330

    :hammer: chore: create constructor and to string

--- core/Form/Field.php
+++ core/Form/Field.php
@@ -22,5 +22,28 @@ class Field
     public RegisterModel $model;
     public string $attribute;
 
- 
+    public string $type;
+
+    public function __construct(RegisterModel $model, string $attribute,$type)
+    {
+        $this->model = $model;
+        $this->attribute = $attribute;
+        $this->type = $type;
+    }
+    public function __toString(): string
+    {
+        return sprintf('
+           <div class="mb-3">
+              <label style="font-size: 23px;font-family:FreeSans,serif;margin-bottom: 5px">%s</label>
+              <input type="%s" name="%s"  value="%s" class="form-control%s">
+              <div class="invalid-feedback">%s</div>
+         </div>
+        ',$this->attribute
+         ,$this->type
+         ,$this->attribute
+         ,$this->model->{$this->attribute}
+         ,$this->model->hasError($this->attribute)? ' is-invalid' : ''
+         ,$this->model->getError($this->attribute)
+        );
+    }
 }
