01f84785d57445197f817ad005a6d6af8c993599
Amirhossein <Amirmemool12@gmail.com>
2023-07-26 23:39:44 +0330

    :hammer: chore: get the db info from the .env

--- public/index.php
+++ public/index.php
@@ -3,8 +3,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\core\Application;
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
-$app = new Application();
+$config = [
+    'db' => [
+        'dns'=>$_ENV['DB_DNS'],
+        'user'=>$_ENV['DB_USER'],
+        'password'=>$_ENV['DB_PASSWORD']
+    ]
+];
+$app = new Application($config);
 
 
 $app->run();
