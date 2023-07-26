743eb11ea37b8e5ce08b831693db041977d72ef7
Amirhossein <Amirmemool12@gmail.com>
2023-07-26 16:47:41 +0330

    :hammer: chore: set error massage for the Rule

--- Rule/RegisterRule.php
+++ Rule/RegisterRule.php
@@ -11,7 +11,6 @@ class RegisterRule extends Rules
     public const MIN_LENGTH = 'min';
     public const MAX_LENGTH = 'max';
     public const MATH_FIELD = 'match';
-    public const UNIQ = 'uniq';
 
     public static function rules(): array
     {
@@ -21,6 +20,17 @@ class RegisterRule extends Rules
             'email' => [self::REQUIRED_FIELD, self::EMAIL],
             'password' => [self::REQUIRED_FIELD, [self::MIN_LENGTH, 'min' => 8], [self::MAX_LENGTH, 'max' => 24]],
             'confirmPassword' => [self::REQUIRED_FIELD, [self::MATH_FIELD, 'match' => 'password']]
+        ];
+    }
+
+    public static function errorMassage(): array
+    {
+        return [
+            self::REQUIRED_FIELD => 'This field is required',
+            self::EMAIL => 'This field must be a valid email address',
+            self::MIN_LENGTH => 'The length of this field must be more than {min}',
+            self::MAX_LENGTH => 'The length of this field must be less than {max}',
+            self::MATH_FIELD => 'This field must be match with {match}',
         ];
     }
 
