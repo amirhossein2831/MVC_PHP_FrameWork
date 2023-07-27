3cc62e4df595b342faabc9feb7f9cb98cef14638
Amirhossein <Amirmemool12@gmail.com>
2023-07-27 18:49:43 +0330

    :hammer: chore: applyMigratoins

--- Migrations/M002_addPassword.php
+++ Migrations/M002_addPassword.php
@@ -2,15 +2,21 @@
 
 namespace App\Migrations;
 
+use PDO;
+
 class M002_some
 {
+    private PDO $pdo;
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
     public function up()
     {
-        echo "up migration";
     }
 
     public function down()
     {
-        echo "down migration";
     }
 }
--- core/DataBase.php
+++ core/DataBase.php
@@ -2,38 +2,44 @@
 
 namespace App\core;
 
-use App\Migrations\M001_Initial;
 use PDO;
-use ReflectionClass;
-use ReflectionException;
 
 class DataBase
 {
     private PDO $pdo;
 
-    public function __construct(array $config){
+    public function __construct(array $config)
+    {
         $dns = $config['dns'] ?? '';
         $user = $config['user'] ?? '';
         $password = $config['password'] ?? '';
-
         $this->pdo = new PDO($dns, $user, $password);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    public function applyMigration()
+    public function applyMigration(): void
     {
         $this->createMigrationTable();
         $appliedMigration = $this->getAppliedMigration();
+
+        $newMigrations = [];
         $files = scandir(Application::$ROOT . '/Migrations');
-        $toApplyMigration = array_diff($files, ['.','..',...$appliedMigration]);
-        
+        $toApplyMigration = array_diff($files, ['.', '..', ...$appliedMigration]);
+
         foreach ($toApplyMigration as $migration) {
             $class = 'App\Migrations\class';
             $migrationClass = pathinfo($migration, PATHINFO_FILENAME);
-            $class =  str_replace('class', $migrationClass,$class);
-            $instance = new $class();
+            $class = str_replace('class', $migrationClass, $class);
+            $instance = new $class($this->pdo);
+            $this->log('Migration is Applying');
             $instance->up();
+            $this->log('Migration is Applied');
+            $newMigrations[] = $migration;
         }
+        if (!empty($newMigrations)) {
+            $this->saveMigration($newMigrations);
+        } else
+            $this->log('All Migration Applied');
     }
 
     public function createMigrationTable(): void
@@ -42,7 +48,7 @@ class DataBase
                                     id INT AUTO_INCREMENT PRIMARY KEY,
                                     migration VARCHAR(255),
                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
-                                    ENGINE=INNODB;");
+                                     ENGINE=INNODB;");
     }
 
     private function getAppliedMigration(): false|array
@@ -51,4 +57,6 @@ class DataBase
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_COLUMN);
     }
+
+
 }
