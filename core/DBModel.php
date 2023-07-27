34a734b81b5bf121d6248616216bd1876fa03f7e
Amirhossein <Amirmemool12@gmail.com>
2023-07-27 22:19:15 +0330

    :hammer: chore: save the user in DB

--- core/DBModel.php
+++ core/DBModel.php
@@ -2,12 +2,29 @@
 
 namespace App\core;
 
+use PDO;
+
 abstract class DBModel extends BaseModel
 {
     protected abstract function DBName(): string;
+    protected abstract function column(): array;
 
-    public function save(){
-
+    public function save(): true
+    {
+        $tableName = $this->DBName();
+        $attributes = $this->column();
+        $param = implode(",", array_map(fn($str) => ":$str", $attributes));
+        $columns = implode(",", $attributes);
+        $statement = $this->getPdo()->prepare("INSERT INTO $tableName ($columns) VALUES($param);");
+        foreach ($attributes as $attribute) {
+            $statement->bindValue(":$attribute", $this->{$attribute});
+        }
+        $statement->execute();
+        return true;
     }
 
+    public function getPdo(): PDO
+    {
+        return Application::$app->getDataBase()->getPdo();
+    }
 }
