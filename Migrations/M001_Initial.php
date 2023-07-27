4cf5d8604d1b0a7bb4e519b917775ea7ccfb849e
Amirhossein <Amirmemool12@gmail.com>
2023-07-27 18:54:18 +0330

    :hammer: chore: implement the up and down method

--- Migrations/M001_Initial.php
+++ Migrations/M001_Initial.php
@@ -1,15 +1,35 @@
 <?php
+
 namespace App\Migrations;
+
+use PDO;
 
 class M001_Initial
 {
-    public function up()
+    private PDO $pdo;
+
+    public function __construct($pdo)
     {
-        echo "up migration";
+        $this->pdo = $pdo;
     }
 
-    public function down()
+    public function up(): void
     {
-        echo "down migration";
+        $SQL = "CREATE TABLE users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                email VARCHAR(255) NOT NULL,
+                firstName VARCHAR(255) NOT NULL,
+                lastName VARCHAR(255) NOT NULL,
+                status TINYINT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                ) ENGINE=InnoDB;";
+        $this->pdo->exec($SQL);
+    }
+
+    public function down(): void
+    {
+        $SQL = "DROP TABLE users;";
+
+        $this->pdo->exec($SQL);
     }
 }
