14a3bebcebce6566f3f4354f290821ffcadac189
Amirhossein <Amirmemool12@gmail.com>
2023-07-27 23:16:14 +0330

    :hammer: chore: add status code in response

--- core/Response.php
+++ core/Response.php
@@ -4,6 +4,9 @@ namespace App\core;
 
 class Response
 {
+    public const IN_ACTIVE_STATUS = 0;
+    public const ACTIVE_STATUS = 1;
+    public const DELETED_STATUS = 2;
     public function setStatusCode(int $code): void
     {
         http_response_code($code);
