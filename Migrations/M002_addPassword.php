96833290599d65fd296864c6b3fe4062655293a0
Amirhossein <Amirmemool12@gmail.com>
2023-07-27 18:54:37 +0330

    :hammer: chore: implement the up and down method

--- Migrations/M002_addPassword.php
+++ Migrations/M002_addPassword.php
@@ -4,19 +4,24 @@ namespace App\Migrations;
 
 use PDO;
 
-class M002_some
+class M002_addPassword
 {
     private PDO $pdo;
+
     public function __construct($pdo)
     {
         $this->pdo = $pdo;
     }
 
-    public function up()
+    public function up(): void
     {
+        $SQL = "ALTER TABLE users ADD COLUMN password VARCHAR(512) NOT NULL ";
+        $this->pdo->exec($SQL);
     }
 
-    public function down()
+    public function down(): void
     {
+        $SQL = "ALTER TABLE users DROP COLUMN password;";
+        $this->pdo->exec($SQL);
     }
 }
