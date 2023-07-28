6efb2c30f411fb5009dd70bc9d9bc521aa5ce975
Amirhossein <Amirmemool12@gmail.com>
2023-07-28 16:38:22 +0330

    :hammer: chore: define the return type

--- Controller/SiteController.php
+++ Controller/SiteController.php
@@ -6,22 +6,22 @@ use App\core\Request;
 
 class SiteController extends BaseController
 {
-    public function home()
+    public function home(): void
     {
         $this->renderView( 'home',  'newLayout');            //if you want to pass value to view pass it to render view then pass it to contentofView
     }
 
-    public function contact()
+    public function contact(): void
     {
         $this->renderView('contact','newLayout');
     }
 
-    public function handleContact(Request $request)
+    public function handleContact(Request $request): void
     {
         var_dump($request->getBody());
     }
 
-    public static function notFount()
+    public static function notFount(): void
     {
         include_once Application::$ROOT . "/Views/notFound.php";
     }
