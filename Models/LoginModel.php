4203e5aeac013d2ad2298c8dbe60d31593dfd2a1
Amirhossein <Amirmemool12@gmail.com>
2023-07-29 17:25:00 +0330

    :hammer: chore: implement the labeland validate and the field

--- Models/LoginModel.php
+++ Models/LoginModel.php
@@ -3,17 +3,50 @@
 namespace App\Models;
 
 use App\core\BaseModel;
+use App\core\Rules;
+use App\Rule\LoginRule;
 
 class LoginModel extends BaseModel
 {
+    public string $email;
 
-    protected function rules(): array
+    public string $password;
+
+    public function __construct()
     {
-        // TODO: Implement validate() method.
+        $this->password = '';
+        $this->email = '';
     }
 
-    protected function validate(): bool
+
+    public function rules(): array
     {
-        // TODO: Implement validate() method.
+        return LoginRule::rules();
+    }
+
+    public function validate(): bool
+    {
+        foreach ($this->rules() as $attribute => $rules) {
+            $value = $this->{$attribute};
+
+            foreach ($rules as $rule) {
+                $ruleName = is_string($rule) ? $rule : $rule[0];
+
+                if ($ruleName === Rules::REQUIRED_FIELD && !$value) {
+                    $this->addError($attribute, Rules::REQUIRED_FIELD);
+                }
+                if ($ruleName === Rules::EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $this->addError($attribute, Rules::EMAIL);
+                }
+            }
+        }
+        return !$this->hasError();
+    }
+    public function labels(): array
+    {
+        return [
+            'password' => 'Password',
+            'email' => 'Email',
+        ];
     }
 }
