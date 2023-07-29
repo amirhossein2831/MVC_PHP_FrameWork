56822e21fa6767739f645ce753269c7e78314a77
Amirhossein <Amirmemool12@gmail.com>
2023-07-29 19:52:07 +0330

    :heavy_plus_sign: feat: create new Logout.php file

---
+++ Views/logout.php
@@ -0,0 +1,6 @@
+<h1>logOut</h1>
+
+<form method="post" action="">
+    <button type="submit" name="logout" value="ok">OK</button>
+    <button type="submit" name="logout" value="no">Cancel</button>
+</form>
