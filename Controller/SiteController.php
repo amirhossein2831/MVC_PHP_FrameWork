dd6d0c8740a0f5ce65f3ce2a0e565b6f504c7cc0
Amirhossein <Amirmemool12@gmail.com>
2023-07-30 00:12:46 +0330

    :recycle: chore: remove some :poop: code

--- Controller/SiteController.php
+++ Controller/SiteController.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Controller;
-use App\core\Application;
 use App\core\BaseController;
 use App\core\Request;
 
@@ -8,7 +7,7 @@ class SiteController extends BaseController
 {
     public function home(): void
     {
-        $this->renderView( 'home',  'newLayout');            //if you want to pass value to view pass it to render view then pass it to contentofView
+        $this->renderView( 'home',  'newLayout');
     }
 
     public function contact(): void
@@ -19,11 +18,6 @@ class SiteController extends BaseController
     public function handleContact(Request $request): void
     {
         var_dump($request->getBody());
-    }
-
-    public static function notFount(): void
-    {
-        include_once Application::$ROOT . "/Views/notFound.php";
     }
 
 }
---
+++ Views/error.php
@@ -0,0 +1,7 @@
+    <?php echo $exception->getCode();?>
+    <div class="not-found">
+        <h1 class="display-1">404</h1>
+        <p class="lead">Page not found</p>
+        <p class="mb-5">The requested page could not be found on the server.</p>
+        <a href="/" class="btn btn-primary">Go Back to Home</a>
+    </div>
