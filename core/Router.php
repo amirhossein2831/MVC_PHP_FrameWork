7c699ecb215645f24ae7e38d7beae3f8fb58035e
Amirhossein <Amirmemool12@gmail.com>
2023-07-29 23:51:36 +0330

    :hammer: chore: execute the middleware

--- core/Router.php
+++ core/Router.php
@@ -4,6 +4,7 @@ namespace App\core;
 
 use App\Component\Interface\RouterMethod;
 use App\Controller\SiteController;
+use App\core\Exception\PageNotFoundException;
 
 class Router implements RouterMethod
 {
@@ -24,18 +25,32 @@ class Router implements RouterMethod
         $this->routes ['POST'][$path] = $callback;
     }
 
+    /**
+     * @throws PageNotFoundException
+     */
     public function resolve(): void
     {
+        /**
+         * @var BaseController $controller
+         */
         $request = Application::$app->getRequest();
-        $response = Application::$app->getResponse();
         $path = $request->getPath();
         $method = $request->method();
         $callback = $this->routes[$method][$path] ?? false;
+        BaseController::$action = $callback[1] ?? '';
+        $controller = $callback[0];
+        foreach ($controller->middlewares as $middleware) {
+            $middleware->execute();
+        }
         if (!$callback) {
-            $response->setStatusCode(404);
-            call_user_func([SiteController::class, 'notFount']);
-            exit;
+            throw new PageNotFoundException();
         }
         call_user_func($callback, $request);
+    }
+
+    public function notFound($response): void
+    {
+        $response->setStatusCode(404);
+        call_user_func([SiteController::class, 'notFount']);
     }
 }
