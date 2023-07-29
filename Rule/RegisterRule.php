11b9892b727c446b1dea2f7b7692cbfaa62f035f
Amirhossein <Amirmemool12@gmail.com>
2023-07-29 17:04:02 +0330

    :heavy_plus_sign: chore: create loginRule and implement the rule

--- Rule/RegisterRule.php
+++ Rule/RegisterRule.php
@@ -7,12 +7,6 @@ use App\Models\UserModel;
 
 class RegisterRule extends Rules
 {
-    public const REQUIRED_FIELD = 'required';
-    public const EMAIL = 'email';
-    public const MIN_LENGTH = 'min';
-    public const MAX_LENGTH = 'max';
-    public const MATCH_FIELD = 'match';
-    public const UNIQUE_EMAIL = 'unique';
 
     public static function rules(): array
     {
@@ -24,16 +18,5 @@ class RegisterRule extends Rules
             'confirmPassword' => [self::REQUIRED_FIELD, [self::MATCH_FIELD, 'match' => 'password']]
         ];
     }
-
-    public static function errorMassage(): array
-    {
-        return [
-            self::REQUIRED_FIELD => 'This field is required',
-            self::EMAIL => 'This field must be a valid Email Address',
-            self::MIN_LENGTH => 'The length of this field must be more than {min}',
-            self::MAX_LENGTH => 'The length of this field must be less than {max}',
-            self::MATCH_FIELD => 'This field must be match with {match}',
-            self::UNIQUE_EMAIL => 'The {attribute} is already taken',
-        ];
-    }
+    
 }
