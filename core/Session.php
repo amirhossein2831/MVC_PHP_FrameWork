bd411839f2ac6038db9e2169306a58ab3d302be9
Amirhossein <Amirmemool12@gmail.com>
2023-07-29 18:44:39 +0330

    ':hammer: chore: implement the set get and remove method in Sessoin

--- core/Session.php
+++ core/Session.php
@@ -47,4 +47,19 @@ class Session implements SessionStorage
     {
         return $_SESSION[self::FLASH_KEY][$key]['value'] ?? false;
     }
+
+    public function get($key)
+    {
+        return $_SESSION[$key] ?? false;
+    }
+
+    public function set($key, $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function remove($key)
+    {
+        unset($_SESSION[$key]);
+    }
 }
