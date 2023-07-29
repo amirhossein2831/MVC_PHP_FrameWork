45e1fa658228434955c96c3c86c8304e66e922c6
Amirhossein <Amirmemool12@gmail.com>
2023-07-29 17:47:36 +0330

    :recycle::lipstick: style: change the ui of login

--- Views/login.php
+++ Views/login.php
@@ -1,29 +1,18 @@
-<?php use App\core\Form\Field,App\core\Form\Form,App\core\Application;?>
-<?php $session = Application::$app->getSession();?>
+<?php /** @var $model App\Models\LoginModel */ ?>
+<?php use App\core\Form\Field,App\core\Form\Form;?>
+
 <div id="container" class="wrapper" style="height: auto">
     <div class="form-box login">
         <h2>Login</h2>
-
-        <?php if ($session->getFlash('success')): ?>
-            <div class="alert alert-success" id="successAlert">
-                <span style="text-align: center;color: darkcyan;font-style: italic"><?php echo $session->getFlash('success');?>
-                <br> <p style="font-size: 15px;color: black">now you can login</p></span>
-                <button type="button" class="close-button" onclick="closeAlert()">x</button>
-            </div>
-        <?php endif; ?>
-
+        <?php Form::successRegister();?>
         <?php $form = Form::begin('', 'post'); ?>
+
             <?php echo $form->field($model, 'email',Field::TEXT_FIELD,'mail'); ?>
             <?php echo $form->field($model, 'password',Field::PASSWORD_FIELD,'lock-closed'); ?>
+            <?php $form->rememberMe();?>
+            <?php $form->submitButton("Login");?>
+            <?php $form->loginRegisterRedirect('/register','Register',"Don't have an account?");?>
 
-            <div class="remember-forget">
-                <label ><input type="checkbox">Remember me</label>
-                <a href="">ForgetPassword</a>
-            </div>
-            <button type="submit" class="btn">Login</button>
-            <div class="login-register">
-                <p>Don't have an account?<a href="/register" class="register-link">Register</a></p>
-            </div>
         <?php Form::end(); ?>
     </div>
 </div>
