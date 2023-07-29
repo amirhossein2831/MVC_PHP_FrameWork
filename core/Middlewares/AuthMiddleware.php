44f92d93f9b174d0925e4dcd36145e6fe8e45d5b
Amirhossein <Amirmemool12@gmail.com>
2023-07-29 23:53:01 +0330

    :hammer: chore: implement the execute of meddleware

--- core/Middlewares/AuthMiddleware.php
+++ core/Middlewares/AuthMiddleware.php
@@ -2,16 +2,26 @@
 
 namespace App\core\Middlewares;
 
+use App\core\Application;
+use App\core\BaseController;
+use App\core\Exception\ForbiddenException;
+
 class AuthMiddleware extends BaseMiddleware
 {
     private array $action;
 
-    public function __construct(array $action = []){
+    public function __construct(array $action = [])
+    {
         $this->action = $action;
     }
 
-    protected function execute()
+    /** @throws ForbiddenException */
+    public function execute(): void
     {
-        // TODO: Implement execute() method.
+        if (Application::$app->isGuest()) {
+            if (empty($this->action) || in_array(BaseController::$action, $this->action)) {
+                throw new ForbiddenException();
+            }
+        }
     }
 }
