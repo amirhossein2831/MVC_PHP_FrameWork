c354d230b3684489de36e938ed9dca37ead537a1
Amirhossein <Amirmemool12@gmail.com>
2023-07-30 17:05:00 +0330

    :hammer: chore: implement the validate method

--- Models/ContactModel.php
+++ Models/ContactModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\core\BaseModel;
+use App\core\Rules;
 use App\Rule\ContactRule;
 
 class ContactModel extends BaseModel
@@ -17,7 +18,6 @@ class ContactModel extends BaseModel
         $this->email = '';
     }
 
-
     protected function rules(): array
     {
         return ContactRule::rules();
@@ -25,7 +25,19 @@ class ContactModel extends BaseModel
 
     protected function validate(): bool
     {
-        // TODO: Implement validate() method.
+        foreach ($this->rules() as $attribute => $rules) {
+            $value = $this->{$attribute};
+            foreach ($rules as $rule) {
+                $ruleName = is_string($rule) ? $rule : $rule[0];
+                if ($ruleName === Rules::REQUIRED_FIELD && !$value) {
+                    $this->addErrorForRule($attribute, Rules::REQUIRED_FIELD);
+                }
+                if ($ruleName === Rules::EMAIL && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    $this->addErrorForRule($attribute, Rules::EMAIL);
+                }
+            }
+        }
+        return !$this->hasError();
     }
 
     protected function labels(): array
