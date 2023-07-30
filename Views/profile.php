a1d0be579ee237e2e1fdf2a38afad817d0f20e5b
Amirhossein <Amirmemool12@gmail.com>
2023-07-30 19:32:24 +0330

    :lipstick: style: change the size of profile div

--- Views/profile.php
+++ Views/profile.php
@@ -1,6 +1,6 @@
 <?php use App\core\Form\Form,App\core\Application; ?>
 <?php $model = Application::$app->getUser(); ?>
-<div id="container" class="wrapper" style="height: auto;text-align: center;width: auto">
+<div id="container" class="wrapper" style="height: auto;text-align: center;width: 500px">
     <div class="profile-container">
         <div class="profile-header"><h1>Profile</h1></div>
         <?php Form::profileField('FirstName',$model->firstName);?>
