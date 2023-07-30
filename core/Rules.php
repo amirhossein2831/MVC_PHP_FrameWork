65e4a7428b49a4e9f2c20460ba32e9b07d6621ab
Amirhossein <Amirmemool12@gmail.com>
2023-07-30 19:12:11 +0330

    :memo: doc: add documentation for Rule

--- core/Rules.php
+++ core/Rules.php
@@ -10,17 +10,26 @@ abstract class Rules
     public const MAX_LENGTH = 'max';
     public const MATCH_FIELD = 'match';
     public const UNIQUE_EMAIL = 'unique';
+
+    /**
+     * @return array
+     */
     public abstract static function rules(): array;
 
-    public static function errorMassage(): array{
-            return [
-                self::REQUIRED_FIELD => 'This field is required',
-                self::EMAIL => 'This field must be a valid Email Address',
-                self::MIN_LENGTH => 'The length of this field must be more than {min}',
-                self::MAX_LENGTH => 'The length of this field must be less than {max}',
-                self::MATCH_FIELD => 'This field must be match with {match}',
-                self::UNIQUE_EMAIL => 'The {attribute} is already taken',
-            ];
+    /**
+     * set a massage for every Rule
+     * @return string[]
+     */
+    public static function errorMassage(): array
+    {
+        return [
+            self::REQUIRED_FIELD => 'This field is required',
+            self::EMAIL => 'This field must be a valid Email Address',
+            self::MIN_LENGTH => 'The length of this field must be more than {min}',
+            self::MAX_LENGTH => 'The length of this field must be less than {max}',
+            self::MATCH_FIELD => 'This field must be match with {match}',
+            self::UNIQUE_EMAIL => 'The {attribute} is already taken',
+        ];
     }
 
 }
