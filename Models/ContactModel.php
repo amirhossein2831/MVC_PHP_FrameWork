25b7d3d4f6afcfdb0c555d6b1c91ec5dda113374
Amirhossein <Amirmemool12@gmail.com>
2023-07-30 17:01:22 +0330

    :hammer: chore: implement constructor rules and validate method in Contact model

--- Models/ContactModel.php
+++ Models/ContactModel.php
@@ -2,7 +2,38 @@
 
 namespace App\Models;
 
-class ContactModel
-{
+use App\core\BaseModel;
+use App\Rule\ContactRule;
 
+class ContactModel extends BaseModel
+{
+    private int $id;
+    private string $subject;
+    private string $descripion;
+    private string $email;
+    public function __construct(){
+        $this->subject = '';
+        $this->descripion = '';
+        $this->email = '';
+    }
+
+
+    protected function rules(): array
+    {
+        return ContactRule::rules();
+    }
+
+    protected function validate(): bool
+    {
+        // TODO: Implement validate() method.
+    }
+
+    protected function labels(): array
+    {
+        return [
+            'subject' => 'Enter Subject',
+            'email' => 'Email',
+            'description' => 'Enter Description'
+        ];
+    }
 }
