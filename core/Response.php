db722a0546c95b574c2e4d73fb37033943c1869e
Amirhossein <Amirmemool12@gmail.com>
2023-07-30 19:08:45 +0330

    :memo: doc: add documentation for Response

--- core/Response.php
+++ core/Response.php
@@ -7,11 +7,20 @@ class Response
     public const IN_ACTIVE_STATUS = 0;
     public const ACTIVE_STATUS = 1;
     public const DELETED_STATUS = 2;
+
+    /**
+     * @param int $code
+     * @return void
+     */
     public function setStatusCode(int $code): void
     {
         http_response_code($code);
     }
 
+    /**
+     * @param $url
+     * @return void
+     */
     public function redirect($url): void
     {
         header("Location: " . $url);
