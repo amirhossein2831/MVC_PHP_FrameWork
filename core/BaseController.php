f2b3063b047e7a6852c4d61f16d94f84e8e885ad
Amirhossein <Amirmemool12@gmail.com>
2023-07-30 16:44:32 +0330

    :recycle: delelte:delete render method from the Base controller

--- core/BaseController.php
+++ core/BaseController.php
@@ -12,28 +12,10 @@ abstract class BaseController
     public array $middlewares = [];
     public static string $action = '';
 
-    public function renderView($view, $layout, $param = []): void
-    {
-        $layoutContent = $this->contentOfLayout($layout);
-        $viewContent = $this->contentOfView($view, $param);
-        echo str_replace('{{content}}', $viewContent, $layoutContent);
-    }
+    protected View $view;
 
-    private function contentOfView($view, $param = []): false|string
-    {
-        foreach ($param as $key => $value) {
-            $$key = $value;
-        }
-        ob_start();
-        include_once Application::$ROOT . "/Views/$view.php";
-        return ob_get_clean();
-    }
-
-    private function contentOfLayout($layout): false|string
-    {
-        ob_start();
-        include_once Application::$ROOT . "/Views/layout/$layout.php";
-        return ob_get_clean();
+    public function __construct($view){
+        $this->view = $view;
     }
 
     protected function registerMiddleware(BaseMiddleware $middleware): void
